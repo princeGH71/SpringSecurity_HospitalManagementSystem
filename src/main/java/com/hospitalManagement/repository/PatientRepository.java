cc63bcda5d816c1485c6bbad16e7125541deacbc
princeGH71 <princesoni7171@gmail.com>
2026-01-18 11:37:56 +0530

    Global Exception Handler

--- src/main/java/com/hospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/hospitalManagement/repository/PatientRepository.java
@@ -29,7 +29,7 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
     @Query("select p from Patient p where p.birthDate > :birthDate")
     List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
 
-    @Query("select new com.codingshuttle.youtube.hospitalManagement.dto.BloodGroupCountResponseEntity(p.bloodGroup," +
+    @Query("select new com.hospitalManagement.dto.BloodGroupCountResponseEntity(p.bloodGroup," +
             " Count(p)) from Patient p group by p.bloodGroup")
 //    List<Object[]> countEachBloodGroupType();
     List<BloodGroupCountResponseEntity> countEachBloodGroupType();
